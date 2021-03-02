de0a15141b5d6bffc60a292057fceed8c60d01c2
ghwns4525 <63900477+ghwns4525@users.noreply.github.com>
2021-03-02 12:37:35 +0900

    스크립트 추가(21.3.2)
    기본적인 스크립트 뼈대 추가 작업

---
+++ OneCard/Assets/Script/Card/Jump.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : Card
+{
+    private int jumpCount;
+
+    protected override void Put()
+    {
+        
+    }
+
+    private void OrderJump()
+    {
+
+    }
+}
