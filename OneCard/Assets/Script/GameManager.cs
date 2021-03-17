fd9cee66b8fe10b176a65febd55f1f3e9fc34f72
Nichi72 <money970709@naver.com>
2021-03-17 13:04:29 +0900

    빌드 에러 해결 완료

--- OneCard/Assets/Script/GameManager.cs
+++ OneCard/Assets/Script/GameManager.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
     private CardManager cardManager;
     [SerializeField] private bool orderDirection = true;
+    [SerializeField] private Transform [] spawnPositions;
+    [SerializeField] private GameObject playerProfilePrefab;
+    [SerializeField] private GameObject playerProfileBase;
+
     private int MaxPlayerCount;
     private int playerCount = 0;
     private int currentTurnPlayer;
     private int[] orderPlayers = new int[4];
     [SerializeField] Text orderTest;
 
+    private void Start()
+    {
+        SpawnPlayer();
+        orderPlayers[0] = 1;
+        orderPlayers[1] = 2;
+        orderPlayers[2] = 3;
+        orderPlayers[3] = 4;
+    }
     public void TurnEnd()
     {
         if(orderDirection)
@@ -45,19 +58,23 @@ public class GameManager : MonoBehaviour
         orderDirection = !orderDirection;
     }
 
-    private void Start()
-    {
-        orderPlayers[0] = 1;
-        orderPlayers[1] = 2;
-        orderPlayers[2] = 3;
-        orderPlayers[3] = 4;
-    }
 
     private void NextCurrentTurnPlayer()
     {
 
     }
 
+    private void SpawnPlayer()
+    {
+        // LocalPlayer : 현재 방에 들어온 로컬 플레이어 (즉 나 자신)
+        // 플레이어 번호를 가져온다.
+        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+        Debug.Log($"localPlayerIndex{localPlayerIndex}");
+        //var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+        //// GameObject.Instantiate() 로컬 세상에서만 생성됨
+        //GameObject profile =  PhotonNetwork.Instantiate(playerProfilePrefab.name, spawnPosition.position, spawnPosition.rotation); // 리소스에서 이름값으로 가져옴. 알아서 동기화를 해준다. 
+        //profile.transform.parent = playerProfileBase.transform;
+    }
 
 
     private void OrderPlayer()
--- OneCard/Assets/Script/Login/Login.cs
+++ OneCard/Assets/Script/Login/Login.cs
@@ -15,7 +15,7 @@ public class Login : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        loginFiled.SetActive(false);
+        //loginFiled.SetActive(false);
     }
 
     // Update is called once per frame
