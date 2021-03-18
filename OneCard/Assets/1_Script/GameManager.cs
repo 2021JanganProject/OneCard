2c98e7e67f06d994802faede7122258adab9790c
Nichi72 <money970709@naver.com>
2021-03-19 01:19:03 +0900

    2020 버전으로 올렸음

---
+++ OneCard/Assets/1_Script/GameManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+// 역할 : 전체적인 게임의 설정과 관리를 담당
+
+public enum eGameFlowState
+{
+    InitPlayer,
+    InitGame,
+    HandOutCards
+}
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager instance = null;
+    public int[] Players { get => orderPlayer; set => orderPlayer = value; }
+    public int MaxPlayerCount { get => MaxPlayerCount; set => MaxPlayerCount = value; }
+
+    [SerializeField] private CardManager cardManager;
+    [SerializeField] private TurnManager turnManager;
+    [SerializeField] private int[]  orderPlayer; // 접속한 순서대로 플레이어가 리스트에 쌓임
+    [SerializeField] private Transform [] spawnPositions;
+    [SerializeField] private GameObject playerProfilePrefab;
+    [SerializeField] private GameObject playerProfileBase;
+
+    private int maxPlayerCount = 4;
+    private int playerCount = 0;
+    private int currentTurnPlayer;
+    private int[] orderPlayers = new int[4];
+
+    private bool isPlayerAllInTheRoom =false;
+    [SerializeField] Text orderTest;
+
+    
+
+    private void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        
+       
+    }
+    IEnumerator SyncPlayerAndPlayerList() // 굳이 이걸 써야하나?
+    {
+        while(isPlayerAllInTheRoom == false)
+        {
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            {
+                orderPlayer[i] = PhotonNetwork.PlayerList[i].ActorNumber-1;
+            }
+            if (maxPlayerCount <= PhotonNetwork.PlayerList.Length)
+            {
+                isPlayerAllInTheRoom = true;
+                break;
+            }
+            yield return null;
+        }
+    }
+    IEnumerator Start()
+    {
+       
+        yield return new WaitForSeconds(1f);
+        SpawnPlayer();
+        StartCoroutine(Update1Sec());
+    }
+    IEnumerator Update1Sec()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            UpdatePlayerListLog();
+        }
+    }
+
+    void UpdatePlayerListLog()
+    {
+        
+        DebugerManager.instance.ResetLog();
+        DebugerManager.instance.Log($"totalplayer: {PhotonNetwork.PlayerList.Length}__playerList : {PhotonNetwork.LocalPlayer.ActorNumber}");
+       
+        for (int i = 0; i < orderPlayer.Length; i++)
+        {
+            DebugerManager.instance.Log($"{PhotonNetwork.PlayerList[i].ActorNumber}__{orderPlayer[i]}");
+        }
+    }
+
+
+    
+    /// <summary>
+    ///  해당 역할은 TrunManager로 이동함
+    /// </summary>
+    public void TurnEnd()
+    {
+        //if (orderDirection)
+        //{
+        //    if (playerCount < 3)
+        //    {
+        //        playerCount += 1;
+        //    }
+        //    else
+        //    {
+        //        playerCount -= 3;
+        //    }
+        //}
+        //else
+        //{
+        //    if (playerCount > 0)
+        //    {
+        //        playerCount -= 1;
+        //    }
+        //    else
+        //    {
+        //        playerCount += 3;
+        //    }
+        //}
+        //orderTest.text = orderPlayers[playerCount].ToString();
+    }
+
+    public void ReverseTurn()
+    {
+        //orderDirection = !orderDirection;
+    }
+
+
+    private void NextCurrentTurnPlayer()
+    {
+
+    }
+    [SerializeField]GameObject playerForTest;
+    private void SpawnPlayer()
+    {
+        // LocalPlayer : 현재 방에 들어온 로컬 플레이어 (즉 나 자신)
+        // 플레이어 번호를 가져온다.
+        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+        Debug.Log($"localPlayerIndex_{localPlayerIndex}");
+        var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+        //// GameObject.Instantiate() 로컬 세상에서만 생성됨
+        playerForTest =  PhotonNetwork.Instantiate(playerProfilePrefab.name, spawnPosition.position, spawnPosition.rotation ); // 리소스에서 이름값으로 가져옴. 알아서 동기화를 해준다. 
+        playerForTest.transform.parent = playerProfileBase.transform;
+        playerForTest.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+
+    private void OrderPlayer()
+    {
+
+    }
+
+
+
+}
